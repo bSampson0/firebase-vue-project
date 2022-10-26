6114fecaa9bec0a64f69e8c808b033b9f7d2a299
b s <bs@bs-MacBook-Pro.attlocal.net>
2022-10-26 14:41:14 -0700

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,18 @@
-<script>
-export default {
-  data() {
-    return {
-      imgRef: null,
-    };
-  },
-  mounted() {
-    this.getRandomDuck();
-  },
-  methods: {
-    getRandomDuck() {
-      fetch("https://random-d.uk/api/v1/random")
-        .then((resp) => resp.json())
-        .then((data) => {
-          console.log(data);
-          this.imgRef = data.url;
-        });
-    },
-  },
-};
-</script>
-
 <template>
-  <main>
-    <img
-      v-if="imgRef"
-      :src="imgRef"
-      alt="this is a random duck!"
-      height="300"
-    />
-    <button @click="getRandomDuck">Get Random Duck!</button>
-  </main>
+  <HelloWorld />
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
